5f3c0b95f055a74a4e6d9d3f33e9794a32ddcd53
giovanni <giovannicachon@gmail.com>
2026-03-08 13:54:06 -0600

    archivo 9 y 10

---
+++ 10_operadores.py
@@ -0,0 +1,19 @@
+#operadores
+
+#Asignación
+x = 5 #asignación simple
+print(x)
+x += 2 #asignación con suma, equivalente a x = x + 2
+print(x)
+x -= 3 #asignación con resta, equivalente a x = x - 3
+print(x)
+x *= 4 #asignación con multiplicación, equivalente a x = x * 4
+print(x)
+x /= 2 #asignación con división, equivalente a x = x / 2
+print(x)
+x //= 3 #asignación con división entera, equivalente a x = x // 3
+print(x)
+x %= 2 #asignación con módulo, equivalente a x = x % 2
+print(x)    
+x **= 2 #asignación con potencia, equivalente a x = x ** 2
+print(x)
---
+++ 9_operadores.py
@@ -0,0 +1,25 @@
+#operadores aritméticos
+
+x = 5
+y = 2
+print(x + y) #suma
+print(x - y) #resta
+print(x * y) #multiplicación
+print(x / y) #división retorna flotante
+print(x // y) #división entera
+print(21 % 2) #módulo
+#modulo es el resto de la división
+print(x ** 2) #potencia
+
+#precedencia de operadores
+print(2 + 3 * 4) #multiplicación tiene mayor precedencia que la suma
+print((2 + 3) * 4) #los paréntesis cambian la precedencia de los operadores
+
+"""
+parentesis
+exponentes
+mult, divisiones
+sumas y restas
+comparaciones de identidad y pertenencia
+logicos
+"""
