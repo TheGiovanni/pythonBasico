8fe047f6623e3f74868066bf2874297ac8feb8cb
giovanni <giovannicachon@gmail.com>
2026-03-11 20:00:02 -0600

    archivo 11 y 12-curso básico

---
+++ 11-operadores_logicos.py
@@ -0,0 +1,10 @@
+#operadores de comparación
+x = 5
+y = 10
+print (x == y) #igualdad False
+print (x != y) #desigualdad  True
+print (x > y) #mayor que False
+print (x < y) #menor que True
+print (x >= y) #mayor o igual que False
+print (x <= y) #menor o igual que True
+
---
+++ 12_condicionales.py
@@ -0,0 +1,13 @@
+#condicionales
+x=5
+y=4
+z= 5
+if x > y and x > z:
+    print("x es mayor que y, y también es mayor que z")
+elif x == y and x == z:
+    print("x es igual a y, y también es igual a z")
+elif x < y or x < z:
+    print("x es menor que y,o x es menor que z")
+else:
+    print("No se cumple ninguna de las condiciones anteriores")
+
